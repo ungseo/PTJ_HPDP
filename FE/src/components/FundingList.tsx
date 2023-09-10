c6eb4d26ddb6b4e6cb2b68dd42557e496979a8a5
origin <mins010437@gmail.com>
2023-09-10 23:18:31 +0900

    [FEAT] 펀딩리스트 컴포넌트 설정

--- FE/src/components/FundingList.tsx
+++ FE/src/components/FundingList.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import FundingItem from './FundingItem';
 
 const FundingList = () => {
   return (
+    <>
     <div>
-      FundingList Component입니다아아아
+      총 1345개의 펀딩이 진행중 입니다
     </div>
+    <FundingItem/>
+    <FundingItem/>
+    <FundingItem/>
+    <FundingItem/>
+    <FundingItem/>
+    
+    </>
   )
 }
 
