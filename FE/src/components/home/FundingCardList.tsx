1c9c554b373c1a0e07b551f9a4862727ffa59148
장혜원 <mins0104@naver.com>
2023-09-14 15:02:50 +0900

    [STYLE] 홈 화면 swiper css 구현

--- FE/src/components/home/FundingCardList.tsx
+++ FE/src/components/home/FundingCardList.tsx
@@ -10,34 +10,35 @@ import "swiper/css/pagination";
 import FundingCard from "./FundingCard";
 
 const FundingCardList = () => {
+  const fundingCards = [
+    { id: 1, title: "Funding Card 1" },
+    { id: 2, title: "Funding Card 2" },
+    { id: 3, title: "Funding Card 3" },
+    { id: 4, title: "Funding Card 4" },
+    { id: 5, title: "Funding Card 5" },
+  ];
+
   return (
     <>
       <Swiper
         // {TypeScript 타입 체크를 우회하기 위한 목적}
         {...{
-          slidesPerView: 2,
+          slidesPerView: "auto",
+          spaceBetween: 1,
           freeMode: true,
+          slidesOffsetBefore: 10,
+          slidesOffsetAfter: 10,
           pagination: {
             clickable: true,
           },
         }}
         className={style.mySwiper}
       >
-        <SwiperSlide>
-          <FundingCard />
-        </SwiperSlide>
-        <SwiperSlide>
-          <FundingCard />
-        </SwiperSlide>
-        <SwiperSlide>
-          <FundingCard />
-        </SwiperSlide>
-        <SwiperSlide>
-          <FundingCard />
-        </SwiperSlide>
-        <SwiperSlide>
-          <FundingCard />
-        </SwiperSlide>
+        {fundingCards.map((card) => (
+          <SwiperSlide key={card.id} style={{ width: "unset" }}>
+            <FundingCard />
+          </SwiperSlide>
+        ))}
       </Swiper>
     </>
   );
