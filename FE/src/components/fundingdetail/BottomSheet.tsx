f50db0b62f6c559d4e68a39bfc0776766efd35c2
장혜원 <mins0104@naver.com>
2023-09-15 09:56:26 +0900

    [STYLE] 펀딩 리스트 css

--- FE/src/components/fundingdetail/BottomSheet.tsx
+++ FE/src/components/fundingdetail/BottomSheet.tsx
@@ -1,39 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Icon } from "@iconify/react";
+import style from "../../styles/css/BottomSheet.module.css";
 
 interface BottomSheetProps {
-    setIsBottomSheetOpen: (value: boolean) => void;
-    handleDonationAmount: (value: number) => void;
+  setIsBottomSheetOpen: (value: boolean) => void;
+  handleDonationAmount: (value: number) => void;
 }
 
-const BottomSheet = ({ setIsBottomSheetOpen, handleDonationAmount }: BottomSheetProps) => {
-    const [donationAmount, setDonationAmount] = useState('');
+const BottomSheet = ({
+  setIsBottomSheetOpen,
+  handleDonationAmount,
+}: BottomSheetProps) => {
+  const [donationAmount, setDonationAmount] = useState("");
 
-    const handleClose = () => {
-        setIsBottomSheetOpen(false);
-    }
+  const handleClose = () => {
+    setIsBottomSheetOpen(false);
+  };
 
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setDonationAmount(e.target.value);
-        handleDonationAmount(Number(e.target.value));
-    }
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDonationAmount(e.target.value);
+    handleDonationAmount(Number(e.target.value));
+  };
 
-    return (
-        <div style={{margin:"2rem"}}>
-            후원 모달
-            <Icon icon="bi:x-circle" onClick={handleClose} />
-            
-            <div>
-                <input 
-                    type="text" 
-                    placeholder="후원할 금액"
-                    value={donationAmount}
-                    onChange={handleInputChange}
-                />
-            </div>
-            
-        </div>
-    );
+  return (
+    <div className={style.bottomsheetcontainer} style={{ margin: "2rem" }}>
+      후원 모달
+      <Icon icon="bi:x-circle" onClick={handleClose} />
+      <div>
+        <input
+          type="text"
+          placeholder="후원할 금액"
+          value={donationAmount}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div style={{ height: "20rem" }}></div>
+    </div>
+  );
 };
 
 export default BottomSheet;
