f342e45f8166d6d9fdcc26733cb2822ce79c00c8
박태규 <everlastin13@naver.com>
2023-09-17 15:12:44 +0900

    [STYLE] 알림 화면 구성 수정

--- FE/src/components/ActivityItem.tsx
+++ FE/src/components/ActivityItem.tsx
@@ -7,14 +7,16 @@ const ActivityItem = () => {
   const navigate = useNavigate();
 
   return (
-    <Grid container className={style.total}>
-      <Grid item xs={3} className={style.imgheight}>
+    <Grid container>
+      <Grid item xs={3} className={style.imgfield}>
         <div className={style.img}>xs=3</div>
       </Grid>
-      <Grid item xs={9} className={style.contentheight}>
-        <div className={style.content}>1</div>
-        <div className={style.content}>2</div>
-        <div className={style.content}>3</div>
+      <Grid item xs={9} className={style.contentfield}>
+        <div className={style.contentbox}>
+          <div className={style.content}>2023.09.01</div>
+          <div className={style.content}>깨끗한 바다와~</div>
+          <div className={style.content}>김웅서님의 1,000포인트가~ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ</div>
+        </div>
       </Grid>
     </Grid>
   )
--- FE/src/components/MessageItem.tsx
+++ FE/src/components/MessageItem.tsx
@@ -7,14 +7,16 @@ const MessageItem = () => {
   const navigate = useNavigate();
   
   return (
-    <Grid container className={style.total}>
-      <Grid item xs={3} className={style.imgheight}>
+    <Grid container>
+      <Grid item xs={3} className={style.imgfield}>
         <div className={style.img}>xs=3</div>
       </Grid>
-      <Grid item xs={9} className={style.contentheight}>
-        <div className={style.content}>1</div>
-        <div className={style.content}>2</div>
-        <div className={style.content}>3</div>
+      <Grid item xs={9} className={style.contentfield}>
+        <div className={style.contentbox}>
+          <div className={style.content}>2023.09.01</div>
+          <div className={style.content}>NoPlasticSunday에서~</div>
+          <div className={style.content}>안녕하세요, 김웅서님.~ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ</div>
+        </div>
       </Grid>
     </Grid>
   )
