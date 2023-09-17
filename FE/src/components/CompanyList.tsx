41361b84b138e868c8f979b37532718502b58dbb
박태규 <everlastin13@naver.com>
2023-09-17 17:39:00 +0900

    [STYLE] 기업 조회 화면 수정

--- FE/src/components/CompanyList.tsx
+++ FE/src/components/CompanyList.tsx
@@ -1,19 +1,64 @@
-import React from 'react';
+import * as React from 'react';
+import ImageList from '@mui/material/ImageList';
 import CompanyItem from './CompanyItem';
-import style from '../styles/css/CompanyList.module.css';
 
-const CompanyList = () => {
+export default function CompanyList() {
   return (
-    <>
-    <div>321개의 기업이 함께하고 있습니다</div>
-      <div className={style.companyListContainer}>
-        <div className={style.companyItem}><CompanyItem/></div>
-        <div className={style.companyItem}><CompanyItem/></div>
-        <div className={style.companyItem}><CompanyItem/></div>
-        <div className={style.companyItem}><CompanyItem/></div>
-      </div>
-    </>
-  )
+    <ImageList>
+      {itemData.map((item, index) => (
+        <CompanyItem key={index} item={item} />
+      ))}
+    </ImageList>
+  );
 }
 
-export default CompanyList;
+const itemData = [
+  {
+    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+    title: 'Breakfast'
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+    title: 'Burger'
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+    title: 'Camera'
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+    title: 'Coffee'
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+    title: 'Hats'
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+    title: 'Honey'
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+    title: 'Basketball'
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+    title: 'Fern'
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+    title: 'Mushrooms'
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+    title: 'Tomato basil'
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+    title: 'Sea star'
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+    title: 'Bike'
+  },
+];
