4a96ee8424daa231a8ed0070ce2047245246dc44
박태규 <everlastin13@naver.com>
2023-09-20 09:51:16 +0900

    [STYLE] 기업 상세 화면 수정

--- FE/src/components/CompanySituation.tsx
+++ FE/src/components/CompanySituation.tsx
@@ -1,34 +1,40 @@
 import React from 'react';
-
 import style from '../../src/styles/scss/CompanySituation.module.scss'
-import Grid from '@mui/material/Unstable_Grid2';
+import Grid from '@mui/material/Grid';
+import FundingItem from './FundingItem';
 
 const CompanySituation = () => {
     return (
         <div>
-            <h1>모금 현황</h1>
-            <Grid container className={style.container}>
-                <Grid xs={6} className={style.item}>
-                    <div>프로젝트 개수</div>
+            <div className={style.text}>모금 현황</div>
+            <Grid container className={style.total}>
+                <Grid item xs={1.5}>
                 </Grid>
-                <Grid xs={6}>
-                    <div>2개</div>
+                <Grid item xs={9} className={style.container}>
+                    <div className={style.item}>
+                        <div>프로젝트 개수</div>
+                        <div>gksk</div>
+                    </div>
+                    <div className={style.item}>
+                        <div>후원 인원</div>
+                        <div>gksk</div>
+                    </div>
+                    <div className={style.item}>
+                        <div>모금 개수</div>
+                        <div>gksk</div>
+                    </div>
+                <Grid item xs={1.5} className={style.height}>
                 </Grid>
-                <Grid xs={6} className={style.item}>
-                    <div>참여 인원</div>
-                </Grid>
-                <Grid xs={6}>
-                    <div>4명</div>
-                </Grid>
-                <Grid xs={6} className={style.item}>
-                    <div>모금 금액</div>
-                </Grid>
-                <Grid xs={6}>
-                    <div>6원</div>
                 </Grid>
             </Grid>
-            <h1>진행 내역</h1>
-            <h1>종료 내역</h1>
+            <div className={style.text}>진행 내역</div>
+            {[1, 1].map((item) => (
+                <FundingItem key={item} />
+            ))}
+            <div className={style.text}>종료 내역</div>
+            {[1, 1].map((item) => (
+                <FundingItem key={item} />
+            ))}
         </div>
     );
 };
