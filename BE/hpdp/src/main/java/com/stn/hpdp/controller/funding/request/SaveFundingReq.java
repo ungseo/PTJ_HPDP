40a8331544ddd34d654eac7a66162dbf1a9cb282
byunyc124 <byunyc124@naver.com>
2023-09-20 11:23:16 +0900

    [FEAT] s3 uploader 추가

--- BE/hpdp/src/main/java/com/stn/hpdp/controller/funding/request/SaveFundingReq.java
+++ BE/hpdp/src/main/java/com/stn/hpdp/controller/funding/request/SaveFundingReq.java
@@ -1,7 +1,5 @@
 package com.stn.hpdp.controller.funding.request;
 
-import com.stn.hpdp.common.enums.BankCode;
-import com.stn.hpdp.common.enums.FundingState;
 import com.stn.hpdp.model.entity.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -9,10 +7,7 @@ import lombok.Getter;
 import lombok.ToString;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.constraints.NotNull;
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.UUID;
 
 @Getter
 @Builder
@@ -21,8 +16,8 @@ import java.util.UUID;
 public class SaveFundingReq {
 
     private String companyLoginId;
-//    private MultipartFile thumbnail;
-//    private MultipartFile content;
+    private MultipartFile thumbnail;
+    private MultipartFile content;
     private String hashtag;
     private String title;
     private String targetAmount;
@@ -32,7 +27,7 @@ public class SaveFundingReq {
     private String rewardName;
     private String rewardPrice;
     private String rewardDesc;
-//    private MultipartFile rewardImg;
+    private MultipartFile rewardImg;
 
     private List<Budget> budgetList;
 
