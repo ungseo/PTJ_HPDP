a0b875eaec8d7ffce2a6449809450fd1d2549d45
장혜원 <mins0104@naver.com>
2023-09-22 09:18:32 +0900

    [FEAT] 회사의 펀딩 목록 확인하기

--- FE/src/api/funding.tsx
+++ FE/src/api/funding.tsx
@@ -17,3 +17,22 @@ export async function getFundingTotalList(
 ) {
   await api.get("").then(success).catch(fail);
 }
+
+export async function getFundingProgress(
+  companyid: number | null,
+  done: number | null,
+  success: (
+    res: AxiosResponse<any, any>
+  ) =>
+    | AxiosResponse<any, any>
+    | PromiseLike<AxiosResponse<any, any>>
+    | null
+    | undefined
+    | void,
+  fail: (err: any) => PromiseLike<never> | null | undefined | void
+) {
+  let apiUrl = "";
+  apiUrl += `?companyId=${companyid}`;
+  apiUrl += `&done=${done}`;
+  await api.get(apiUrl).then(success).catch(fail);
+}
