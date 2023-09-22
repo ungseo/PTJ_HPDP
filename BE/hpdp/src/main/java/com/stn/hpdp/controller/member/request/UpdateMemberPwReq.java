087c98c0445359d90e37c4df602906fc4113a7eb
jeonghun98 <wjdgns7040@gmail.com>
2023-09-22 12:17:38 +0900

    [FEAT] 개인정보 수정 API 구현

---
+++ BE/hpdp/src/main/java/com/stn/hpdp/controller/member/request/UpdateMemberPwReq.java
@@ -0,0 +1,15 @@
+package com.stn.hpdp.controller.member.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
+@Getter
+@Setter
+public class UpdateMemberPwReq {
+    @NotEmpty(message = "비밀번호는 필수 입력값입니다.")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+    private String loginPw;
+}
