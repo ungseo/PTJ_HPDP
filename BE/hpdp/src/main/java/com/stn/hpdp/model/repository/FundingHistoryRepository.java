1c28ae5e5fc937ba47c8158af24225b5c37c10e8
byunyc124 <byunyc124@naver.com>
2023-09-27 13:32:16 +0900

    [FEAT] 메인 페이지 api 추가

--- BE/hpdp/src/main/java/com/stn/hpdp/model/repository/FundingHistoryRepository.java
+++ BE/hpdp/src/main/java/com/stn/hpdp/model/repository/FundingHistoryRepository.java
@@ -2,6 +2,7 @@ package com.stn.hpdp.model.repository;
 
 import com.stn.hpdp.model.entity.FundingHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +14,7 @@ public interface FundingHistoryRepository extends JpaRepository<FundingHistory, 
     FundingHistory findByFunding_Id(Long fundingId);
 
     List<FundingHistory> findAllByFunding_Id(Long fundingId);
+
+    @Query("SELECT SUM(price) FROM FundingHistory")
+    int getSumPrice();
 }
