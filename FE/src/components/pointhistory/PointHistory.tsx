e8a73d6daaf17ae09dc6f3015944dacf130c9785
장혜원 <mins0104@naver.com>
2023-10-02 03:28:51 +0900

    [STYLE] 거래내역 css

--- FE/src/components/pointhistory/PointHistory.tsx
+++ FE/src/components/pointhistory/PointHistory.tsx
@@ -3,19 +3,26 @@ import { pointHistoryInterface } from "./../../interface/transactionHistoryInter
 import style from "../../styles/css/PointHistory.module.css";
 import { useSelector } from "react-redux";
 
+function formatNumber(number: number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 const PointHistory = ({ totalPoint, totalFunding }: pointHistoryInterface) => {
   const isInsert = useSelector((state: any) => state.transHistory.isInsert);
   const styles = { backgroundColor: "#25228C" };
+  const total_Point = formatNumber(totalPoint);
+  const total_Funding = formatNumber(totalFunding);
+
   return (
     <div className={style.wrapper}>
       <div className={style.pointHistory} style={styles}>
         <div className={style.totalPoint}>
           <p>현재 잔여 포인트</p>
-          <p>{totalPoint} P</p>
+          <p>{total_Point} P</p>
         </div>
         <div className={style.yesterdayPoint}>
           <p>전체 후원 금액</p>
-          <p>{totalFunding} P</p>
+          <p>{total_Funding} P</p>
         </div>
       </div>
     </div>
