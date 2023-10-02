90dd1ce62dcda1e309e2ecba246db5e53b90f070
김웅서 <ungseo@gmail.com>
2023-10-02 20:27:11 +0900

    [DESIGN] auth쪽 alert모달, 인가되지 않은 페이지 이동시 잠깐 화면깜빡임 해결, 회원가입 지갑생성시 로딩스피너 추가

--- FE/src/components/profile/ProfileWrapper.tsx
+++ FE/src/components/profile/ProfileWrapper.tsx
@@ -1,11 +1,12 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ProfileActivity from "./ProfileActivity";
 import ProfilePhoto from "./ProfilePhoto";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import LoadingSpinner from "../common/LoadingSpinner";
 const ProfileWrapper = () => {
   const isLogined = useSelector((state: any) => state.user.auth.isLogined);
-
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -15,9 +16,12 @@ const ProfileWrapper = () => {
       console.log("로그인 N");
       navigate("/login");
     }
+    setLoading(false);
   }, []);
 
-  return (
+  return loading ? (
+    <LoadingSpinner />
+  ) : (
     <div>
       <ProfilePhoto />
       <ProfileActivity />
--- FE/src/pages/NotificationPage.tsx
+++ FE/src/pages/NotificationPage.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { OptionTopbar } from "../components/common/TopBar";
 import Activity from "../components/notification/Activity";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import LoadingSpinner from "../components/common/LoadingSpinner";
 
 const NotificationPage = () => {
   // 로그인 Y
@@ -12,17 +13,17 @@ const NotificationPage = () => {
   const navigate = useNavigate();
 
   const isLogined = useSelector((state: any) => state.user.auth.isLogined);
-
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
-    if (isLogined) {
-      console.log("로그인 Y");
-    } else {
-      console.log("로그인 N");
+    if (!isLogined) {
       navigate("/login");
+      setLoading(false);
     }
   }, []);
 
-  return (
+  return loading ? (
+    <LoadingSpinner />
+  ) : (
     <div>
       <OptionTopbar text={"알림"} />
       <div>
