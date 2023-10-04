0a541840e2724c56c4052f0756bdb315aafb5147
박태규 <everlastin13@naver.com>
2023-10-04 17:18:10 +0900

    [FEAT] 기업 상세 화면 관심 추가

--- FE/src/components/DetailPageTop.tsx
+++ FE/src/components/DetailPageTop.tsx
@@ -1,38 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
 import style from "../styles/css/DetailPageTop.module.css";
 import { Icon } from "@iconify/react";
+import IconButton from "@mui/material/IconButton";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import { useSelector } from "react-redux";
+import {
+  registerInterestingCompany,
+  unregisterInterestingCompany,
+} from "../api/interests";
 
 interface DetailTopProps {
   data: {
+    companyId: number;
+    interested?: boolean;
     name: string;
-    title?: string;
-    thumbnail?: string | null;
-    companyId?: number;
     profileImg?: string;
+    thumbnail?: string | null;
+    title?: string;
   };
 }
 
 const DetailPageTop = (props: DetailTopProps) => {
   const { data } = props;
+  console.log("라라", data);
+
+  const companyId = data.companyId;
+
   const navigate = useNavigate();
 
   const handleGoList = () => {
     navigate("/list");
   };
   const handleGoCompany = () => {
-    const { companyId } = data;
     if (companyId) {
       navigate(`/company/detail/${companyId}`);
     }
   };
+
   const totalStyle = {
     backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.4)), url(${data.thumbnail})`,
   };
 
+  // 관심 기업 등록(삭제)
+  const isLogined = useSelector((state: any) => state.user.auth.isLogined);
+
+  const [isLiked, setIsLiked] = useState(data.interested);
+
+  const accessToken = useSelector((state: any) => state.user.auth.accessToken);
+
+  const toggleLike = () => {
+    console.log("기업 번호:", companyId, "관심 여부:", isLiked);
+
+    if (isLiked) {
+      unregisterInterestingCompany(
+        accessToken,
+        companyId,
+        (res) => {
+          setIsLiked(!isLiked);
+          console.log("관심 기업 삭제", res);
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    } else {
+      registerInterestingCompany(
+        accessToken,
+        companyId,
+        (res) => {
+          setIsLiked(!isLiked);
+          console.log("관심 기업 등록", res);
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    }
+  };
+
   return (
     <div className={style.total} style={totalStyle}>
+      {isLogined ? (
+        <IconButton
+          aria-label={`like ${data.name}`}
+          onClick={(event) => {
+            event.stopPropagation();
+            toggleLike();
+          }}
+          className={style.iconButton}
+          style={{
+            color: isLiked ? "red" : "lightgray",
+          }}
+        >
+          <FavoriteIcon />
+        </IconButton>
+      ) : null}
+
       <Icon
         icon="bi:chevron-left"
         className={style.icon}
@@ -40,6 +104,9 @@ const DetailPageTop = (props: DetailTopProps) => {
         onClick={handleGoList}
       />
       <div className={style.topcontent}>
+        <div className={style.downsection}>
+          {data.title && <div className={style.fundingtitle}>{data.title}</div>}
+        </div>
         <div className={style.upsection}>
           <img
             src={data.profileImg}
@@ -49,9 +116,6 @@ const DetailPageTop = (props: DetailTopProps) => {
           <div className={style.companyname} onClick={handleGoCompany}>
             {data.name}
           </div>
-        </div>
-        <div className={style.downsection}>
-          {data.title && <div className={style.fundingtitle}>{data.title}</div>}
         </div>
       </div>
     </div>
--- FE/src/pages/CompanyDetailPage.tsx
+++ FE/src/pages/CompanyDetailPage.tsx
@@ -12,22 +12,16 @@ import { getCompanyItem } from "../api/companies";
 import style from "../styles/css/CompanyDetailPage.module.css";
 
 const CompanyDetailPage = () => {
+  // 상세 조회
+  const { companyid } = useParams();
+
+  const accessToken = useSelector((state: any) => state.user.auth.accessToken);
+
   const [companyItem, setCompanyItem] =
     useState<Interfaces.InSearchCompanyInfoResponseInterface>(
       {} as Interfaces.InSearchCompanyInfoResponseInterface
     );
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const accessToken = useSelector((state: any) => state.user.auth.accessToken);
-  const isLogined = useSelector((state: any) => state.user.auth.isLogined);
-  const { companyid } = useParams();
-
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
+  console.log("허억", companyItem);
 
   useEffect(() => {
     getCompanyItem(
@@ -42,24 +36,36 @@ const CompanyDetailPage = () => {
       }
     );
   }, []);
-  console.log(companyItem);
+
   const tabProps = {
     소개: <CompanyIntroduce item={companyItem} />,
     후원: <CompanySituation item={companyItem} />,
   };
 
+  // 모달 변화
+  const isLogined = useSelector((state: any) => state.user.auth.isLogined);
+
+  const [modalStatus, setModalStatus] = useState(false);
+
+  const changeModal = () => {
+    setModalStatus(!modalStatus);
+  };
+
+  // 상속 정보
   const data = {
+    companyId: companyItem.companyId,
+    interested: companyItem.interested,
     name: companyItem.name,
-    receiverId: Number(companyid),
     profileImg: companyItem.profile,
     thumbnail: companyItem.banner,
   };
+
   return (
     <div className={style.companydetailpage}>
       <DetailPageTop data={data} />
       <CustomizedTabs tabProps={tabProps} />
       {isLogined ? (
-        <div className={style.message_icon} onClick={openModal}>
+        <div className={style.message_icon} onClick={changeModal}>
           <Icon
             icon="bi:chat-square-dots"
             style={{
@@ -71,10 +77,10 @@ const CompanyDetailPage = () => {
         </div>
       ) : null}
 
-      {isModalOpen && (
+      {modalStatus && (
         <>
           <div className={style.modalbackground}></div>
-          <SendMessageModal onClose={closeModal} data={data} />
+          <SendMessageModal onClose={changeModal} data={data} />
         </>
       )}
     </div>
