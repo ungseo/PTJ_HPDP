1cd1ff053d0c70e83dd71af3901d825dcf8f1e12
장혜원 <mins0104@naver.com>
2023-10-05 13:49:09 +0900

    [FEAT] 지갑 주소 설정

--- FE/src/pages/CompanyDetailPage.tsx
+++ FE/src/pages/CompanyDetailPage.tsx
@@ -10,13 +10,17 @@ import { Icon } from "@iconify/react";
 import * as Interfaces from "../interface/apiDataInterface";
 import { getCompanyItem } from "../api/companies";
 import style from "../styles/css/CompanyDetailPage.module.css";
+import { getCompanyBlockchainInfo } from "../api/blockchain";
+import WalletAddress from "../components/common/WalletAddress";
 
 const CompanyDetailPage = () => {
   // 상세 조회
   const { companyid } = useParams();
 
   const accessToken = useSelector((state: any) => state.user.auth.accessToken);
-
+  const [companyBlockChainInfo, setCompanyBlockChainInfo] =
+    useState<string>("");
+  const [isWalletModalOpen, setWalletModalOpen] = useState(false);
   const [companyItem, setCompanyItem] =
     useState<Interfaces.InSearchCompanyInfoResponseInterface>(
       {} as Interfaces.InSearchCompanyInfoResponseInterface
@@ -36,6 +40,19 @@ const CompanyDetailPage = () => {
     );
   }, []);
 
+  useEffect(() => {
+    getCompanyBlockchainInfo(
+      accessToken,
+      Number(companyid),
+      (res) => {
+        setCompanyBlockChainInfo(res.data.data);
+      },
+      (err) => {
+        console.error("API 호출 실패:", err);
+      }
+    );
+  }, []);
+
   const tabProps = {
     소개: <CompanyIntroduce item={companyItem} />,
     후원: <CompanySituation item={companyItem} />,
@@ -45,7 +62,9 @@ const CompanyDetailPage = () => {
   const isLogined = useSelector((state: any) => state.user.auth.isLogined);
 
   const [modalStatus, setModalStatus] = useState(false);
-
+  const handleWalletButtonClick = () => {
+    setWalletModalOpen(!isWalletModalOpen);
+  };
   const changeModal = () => {
     setModalStatus(!modalStatus);
   };
@@ -60,6 +79,13 @@ const CompanyDetailPage = () => {
 
   return (
     <div className={style.companydetailpage}>
+      <div
+        className={style.walletIcon}
+        onClick={handleWalletButtonClick}
+        style={{ color: "white" }}
+      >
+        <Icon icon="bi-wallet2" />
+      </div>
       <DetailPageTop data={data} />
 
       <CustomizedTabs tabProps={tabProps} />
@@ -82,6 +108,9 @@ const CompanyDetailPage = () => {
           <div className={style.modalbackground}></div>
           <SendMessageModal onClose={changeModal} data={data} />
         </>
+      )}
+      {isWalletModalOpen && (
+        <WalletAddress memberBlockChainInfo={companyBlockChainInfo} />
       )}
     </div>
   );
